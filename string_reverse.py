c0bba2388042fd3ff0dea8a039031f7b9404f942
Binay Raut <binayraut8@gmail.com>
2024-08-17 12:25:10 -0700

    updates to string reverse file

--- string_reverse.py
+++ string_reverse.py
@@ -1,6 +1,6 @@
 class solution:
     def __init__(self, s) -> None:
-        input_string = s
+        input_string = s # this is a placeholder comment
     
     def ReverseString(self,s):
         return(s[::-1])
